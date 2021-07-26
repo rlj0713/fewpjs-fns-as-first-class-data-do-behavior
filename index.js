12b21b6ea0dc5bcd1ef2aa1673a751eb95f55e8a
Reid <rlj0713@gmail.com>
2021-07-25 20:02:09 -0400

    commit tree f97dddb9a0a42df4243e2e6ad5e9fff0f98844f0

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let number = parseInt(string);
+
+  if (number >= 0 && number <= 12) {
+    return "Good Morning"
+  } else if (number > 12 && number <= 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(input) {
+  document.getElementById("greeting").textContent = input;
+}
